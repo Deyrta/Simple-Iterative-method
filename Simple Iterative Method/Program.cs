7df3f981b45bab4e5830ed50013fcd26fb0f671b
Павел Климко <tuhan4ek@gmail.com>
2019-11-18 16:29:45 +0200

    Добавьте файлы проекта.

---
+++ Simple Iterative Method/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Lab1KMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Simple_Iterative_Method SIM = new Simple_Iterative_Method();
+            SIM.Method();
+            Console.ReadKey();
+        }
+    }
+
+    class Simple_Iterative_Method
+    {
+        private double x;
+        private double function;
+        private double pohybka;
+        private double accuracy;
+
+        public void Method()
+        {
+            Console.WriteLine("Enter a accuracy and first x:");
+            accuracy = double.Parse(Console.ReadLine());
+            x = double.Parse(Console.ReadLine());
+            while (true)
+            {
+                function = (1 - 2 * x * x * x) / 2; // derivative from function
+                pohybka = function - x;
+                x = function;
+                if (pohybka < accuracy)
+                {
+                    Console.WriteLine("Result is:" + x);
+                    break;
+                }
+                else
+                    continue;
+            }
+        }
+    }
+}
